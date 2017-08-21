222b56ccbc5497a320a22eaa48007b02abac57a2
sergiosolorzano <sergiosolorzano@gmail.com>
2017-08-21 19:22:29 +0100

    sound integration. added sfx/music for sampling purposes on build , these sounds would be replaced by teams

---
+++ Assets/FootStepsAudio.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepsAudio : MonoBehaviour {
+
+    [SerializeField] AudioSource myAudio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
+        {
+            myAudio.Play();
+            //Debug.Log("Down Horizontal: Playing footsteps");
+        }
+
+    if (Input.GetButtonDown("Vertical") && Input.GetButtonDown("Vertical"))
+        {
+            myAudio.Play();
+            //Debug.Log("Down Horizontal and Vertical: Playing footsteps");
+        }
+
+    if (Input.GetButtonUp("Horizontal") && !Input.GetButton("Vertical"))
+        {
+            myAudio.Stop();
+            //Debug.Log("Stop Steps");
+        }
+
+    if (Input.GetButtonUp("Vertical") && !Input.GetButton("Horizontal"))
+        {
+            myAudio.Stop();
+            //Debug.Log("Stop Steps");
+        }
+    }
+}
---
+++ Assets/PaintingRemoveAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintingRemoveAudio : MonoBehaviour {
+    [SerializeField] AudioSource myAudio;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Interact"))
+        {
+            myAudio.Play();
+        }
+        
+    }
+}
