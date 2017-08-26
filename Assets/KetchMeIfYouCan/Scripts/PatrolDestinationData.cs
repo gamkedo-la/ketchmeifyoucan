be37f78297f1b54893b3b733f2ea808485a7a43d
Christer Kaitila <github@mcfunkypants.com>
2017-08-26 13:25:52 -0700

    smaller patrol waypoint gizmos in unity editor

--- Assets/KetchMeIfYouCan/Scripts/PatrolDestinationData.cs
+++ Assets/KetchMeIfYouCan/Scripts/PatrolDestinationData.cs
@@ -21,4 +21,19 @@ public class PatrolDestinationData : MonoBehaviour
 		Gizmos.DrawFrustum(Vector3.zero, fov, max, min, 1);
 		Gizmos.matrix = temp;
 	}
+
+	public void OnDrawGizmos() // called only in unity editor
+	{
+		// see https://docs.unity3d.com/ScriptReference/Gizmos.html
+		float fov = 45;
+		float max = 0.25f;
+		float min = 0.1f;
+
+		Matrix4x4 temp = Gizmos.matrix;
+		Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+		Gizmos.color = Color.red;
+		Gizmos.DrawFrustum(Vector3.zero, fov, max, min, 1);
+		Gizmos.matrix = temp;
+	}
+
 }
