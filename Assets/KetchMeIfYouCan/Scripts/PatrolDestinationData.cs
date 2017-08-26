510c49b1ae41b350e2c122ad07f7d8cb646d53f2
Christer Kaitila <github@mcfunkypants.com>
2017-08-26 13:20:33 -0700

    patrol waypoints now have a line of sight gizmo

--- Assets/KetchMeIfYouCan/Scripts/PatrolDestinationData.cs
+++ Assets/KetchMeIfYouCan/Scripts/PatrolDestinationData.cs
@@ -2,8 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PatrolDestinationData : MonoBehaviour
 {
     public float m_WaitTime = 5.0f;
     public string m_GuardDialogue = "";
+
+	public void OnDrawGizmosSelected() // called only in unity editor when this object is selected
+	{
+		// see https://docs.unity3d.com/ScriptReference/Gizmos.html
+		float fov = 45;
+		float max = 5;
+		float min = 0.1f;
+
+		Matrix4x4 temp = Gizmos.matrix;
+		Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+		Gizmos.color = Color.red;
+		Gizmos.DrawFrustum(Vector3.zero, fov, max, min, 1);
+		Gizmos.matrix = temp;
+	}
 }
