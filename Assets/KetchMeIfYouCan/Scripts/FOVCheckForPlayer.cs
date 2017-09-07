f0ee47aaa8b0b9ee38a11bca2ba9cd419457551b
ALEX-XPS\Alex <whisker.ship@gmail.com>
2017-09-07 02:30:56 -0700

    - Made a new level. (test level 0)

---
+++ Assets/KetchMeIfYouCan/Scripts/FOVCheckForPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FieldOfView))]
+public class FOVCheckForPlayer : MonoBehaviour {
+
+    FieldOfView fov;
+    Transform player;
+    private void Awake() {
+
+        fov = GetComponent<FieldOfView>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update() {
+        
+        if (Vector3.Distance(transform.position, player.position) < fov.viewRadius) {
+            if (CheckPlayer()) {
+                GameManager.RestartGame("You're done.");
+            }
+        }
+    }
+
+    //Checks if player is within the field of view.
+    bool CheckPlayer() {
+
+        if (fov.visibleTargets.Contains(player))
+            return true;
+
+        else return false;
+    }
+}
